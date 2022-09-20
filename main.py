c26c77b41fff3a95888a107b682aab2e0df13c86
Nathan Solis <113551835+nasolis665@users.noreply.github.com>
2022-09-20 19:30:45 +0000

    math and boolean

--- main.py
+++ main.py
@@ -5,68 +5,75 @@
 #you define a tuple with () instead of [] like you would in lists
 #once you define it, you cannot change it
 #example
-coordinates = (4, 5)
-# get the first element in the above tuple
-print(coordinates[1])
-# lets make this a multidimensional array
-coordindates2 = [(4, 5), (6, 7), (80, 34)]
-#get the second element of the second item in coordinates2
-print(coordindates2[1][1])
-# we use parentheses not [] in tuples
-my_tuple1 = (1, 2, 3, 4)
-print(my_tuple1[1])
-# get the second item in the tuple1 above
-my_tuple = (1, 2, (10, 20), 4)
-print(my_tuple[2][1])
-# get the second item in the 3rd item above
-#hint multidimensional array thinking
+# coordinates = (4, 5)
+# # get the first element in the above tuple
+# print(coordinates[1])
+# # lets make this a multidimensional array
+# coordindates2 = [(4, 5), (6, 7), (80, 34)]
+# #get the second element of the second item in coordinates2
+# print(coordindates2[1][1])
+# # we use parentheses not [] in tuples
+# my_tuple1 = (1, 2, 3, 4)
+# print(my_tuple1[1])
+# # get the second item in the tuple1 above
+# my_tuple = (1, 2, (10, 20), 4)
+# print(my_tuple[2][1])
+# # get the second item in the 3rd item above
+# #hint multidimensional array thinking
 
-# place these numbers in separate variables from the tuple below
-t = (1, 2, 3)
-x,y,z=t
-print(x,y,z)
-#get me the length of the above tuple --- there are two ways of doing this... len(), count()...  use count if you want to get how many times an item appears in a tuple
-print(len(my_tuple))
-# Use a tuple method to count the number of times the value 2 appears in the following tuple, and display the result (integer) on the screen:
+# # place these numbers in separate variables from the tuple below
+# t = (1, 2, 3)
+# x,y,z=t
+# print(x,y,z)
+# #get me the length of the above tuple --- there are two ways of doing this... len(), count()...  use count if you want to get how many times an item appears in a tuple
+# print(len(my_tuple))
+# # Use a tuple method to count the number of times the value 2 appears in the following tuple, and display the result (integer) on the screen:
 
-my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
-print(my_tuple.count(2))
-# Convert the following tuple to a list, and store it in a variable called my_list.
+# my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+# print(my_tuple.count(2))
+# # Convert the following tuple to a list, and store it in a variable called my_list.
 
-my_tuple = (1, 2, 3, 2, 3, 1, 3, 2)
-my_list=list(my_tuple)
-print(my_list)
-#Extract the elements of the following tuple into four variables: a, b, c, d
+# my_tuple = (1, 2, 3, 2, 3, 1, 3, 2)
+# my_list=list(my_tuple)
+# print(my_list)
+# #Extract the elements of the following tuple into four variables: a, b, c, d
 
-my_tuple = (1, 2, 3, 4)
-a,b,c,d=my_tuple
-print(a)
+# my_tuple = (1, 2, 3, 4)
+# a,b,c,d=my_tuple
+# print(a)
 
-#################################################sets######################################################
+# #################################################sets######################################################
+# #sets are similar to tuples. the only difference is that they will not contain or print out any repeated elements
+# my_firstSet=set((1,2,2,2,3,4,5))
+# print(my_firstSet)
+# print(6 in my_firstSet)
+# # Join the following sets into one, called my_set_3:
 
+# set1={1, 2, "three", "four"}
 
-# Join the following sets into one, called my_set_3:
+# set2={"three", 4, 5}
 
-{1, 2, "three", "four"}
+# s3=set1.union(set2)
+# print(s3)
+# s3.add(8)
+# print(s3)
+# # Remove a random item from the following set, using set methods.
 
-{"three", 4, 5}
+# raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+# raffle.remove("Rachel")
+# print(raffle)
+# raffle.remove("Phoebe")
+# print(raffle)
+# # Add the name Gunther to the following set, using set methods:
 
-
-# Remove a random item from the following set, using set methods.
-
-raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
-
-
-
-# Add the name Gunther to the following set, using set methods:
-
-raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
-
+# raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+# raffle.add("Gunther")
+# print(raffle)
 #################################################booleans######################################################
 # booleans
 # a boolean can only have 2 values -- true or False
-# my_bool = 5 > 4
-#print(my_bool)
+my_bool = 5 > 4
+print(my_bool)
 
 # > greater
 # < less
@@ -76,8 +83,9 @@ raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
 # != different or not equal to
 
 # you can also construct booleans to see if values ar in a variable or not found
-# my_ bool = 5 in my_list
-# my_bool = 5 not in my_list
+my_list=[1,2,3,4,5,6,7]
+my_bool = 5 in my_list
+my_bool = 5 not in my_list
 
 # so we can see if we can make logical decisions if something is true or not
 # var1 = True
@@ -85,19 +93,26 @@ raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
 # print(type(var1))
 # print(var1)
 
-# list = [1,2,3,4,5,6]
-# control = 5 in list
-# print(type(control))
-# print(control)
+list = [1,2,3,4,5,6]
+control = 5 in list
+print(type(control))
+print(control)
 
 # Booleans Practice
 
 # Make a comparison that returns a boolean and store the result (True/False) in a variable called test
-
+num=5==4
+test=num
+print(test)
 # Check if 17834/34 is greater than 87*56 and print the boolean result to the screen using print()
-
+divide=17834/34
+multipy=87*56
+test=divide>multipy
+print(test)
 # Check if the square root of 25 is equal to 5 and display the result (boolean) on the screen using print
-
+import math
+square=math.sqrt(25)
+print(square==5)
 
 
 #############################################next week ################################################
